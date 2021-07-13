53704ce5c0e999b36418568e9fa18745a9203594
Nattamon Polvichai <joppy.pgh@gmail.com>
2021-07-13 23:34:52 +0700

    🔧 add jsconfig and stuff

---
+++ src/components/AppContainer.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: ${(props) => props.theme.colors.neutral[900]};
+`
