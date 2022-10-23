ae79b517a43d2cb74e3e5bbb27a772d4e3eca60d
Nattamon Polvichai <joppy.pgh@gmail.com>
2022-10-23 21:48:15 +0700

    ➕ setup dependencies for connecting to strapi

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,72 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   plugins: [
     `gatsby-plugin-react-svg`,
     `gatsby-plugin-fontawesome-css`,
     `gatsby-plugin-postcss`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [`IBM Plex Sans Thai Looped\:400,400i,700,700i`],
         display: "swap",
+      },
+    },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: process.env.STRAPI_API_URL || "http://localhost:8082",
+        accessToken: process.env.STRAPI_TOKEN,
+        collectionTypes: [
+          {
+            singularName: "article",
+            queryParams: {
+              publicationState:
+                process.env.GATSBY_IS_PREVIEW === "true" ? "preview" : "live",
+              populate: {
+                cover: "*",
+                blocks: {
+                  populate: "*",
+                },
+              },
+            },
+          },
+          {
+            singularName: "author",
+          },
+          {
+            singularName: "column",
+          },
+        ],
+        singleTypes: [
+          {
+            singularName: "about",
+            queryParams: {
+              populate: {
+                blocks: {
+                  populate: "*",
+                },
+              },
+            },
+          },
+          {
+            singularName: "global",
+            queryParams: {
+              populate: {
+                favicon: "*",
+                defaultSeo: {
+                  populate: "*",
+                },
+              },
+            },
+          },
+        ],
       },
     },
   ],
