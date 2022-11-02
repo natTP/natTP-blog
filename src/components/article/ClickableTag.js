30b4d8d61b91459a5e90819449a76cc6f042e144
Nattamon Polvichai <joppy.pgh@gmail.com>
2022-11-03 02:08:17 +0700

    ✏️ add uppercase to tags

--- src/components/article/ClickableTag.js
+++ src/components/article/ClickableTag.js
@@ -6,7 +6,7 @@ function ClickableTag({ tag }) {
     return (
         <Link
             to={`/tag/${stringToSlug(tag.title)}`}
-            className='px-2.5 font-decorative text-base leading-4 text-amethyst-300 bg-amethyst-0
+            className='px-2.5 font-decorative text-base leading-4 text-amethyst-300 bg-amethyst-0 uppercase
 						hover:text-amethyst-700 hover:bg-amethyst-200 focus:text-amethyst-700 focus:bg-amethyst-200 transition ease-out duration-300'
         >
             #{tag.title}
