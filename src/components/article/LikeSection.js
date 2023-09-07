0a9fc788a70653a97a271b8f7a57aa9fc0882905
Nattamon Polvichai <joppy.pgh@gmail.com>
2023-09-08 00:49:03 +0700

    ✨ like button microinteraction

--- src/components/article/LikeSection.js
+++ src/components/article/LikeSection.js
@@ -1,26 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ClapButton } from '@lyket/react'
 import Button from 'components/common/Button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart as faHeartFilled } from '@fortawesome/free-solid-svg-icons'
 import { faHeart as faHeartBordered } from '@fortawesome/free-regular-svg-icons'
 import { faEye } from '@fortawesome/free-regular-svg-icons'
-
-// TODO cute like microinteraction
-// TODO
+import Thanks from 'assets/nat.svg'
 
 function LikeSection({ className, articleId }) {
     return (
         <ClapButton className={className} namespace='article' id={articleId}>
             {({ handlePress, totalClaps, userClaps, isLoading }) => (
-                <div className='flex flex-wrap justify-between items-center gap-1'>
+                <div className='group relative flex flex-wrap justify-between items-center gap-1'>
                     <span className='flex gap-4 items-center'>
                         <span
                             className={`max-w-fit font-loopless font-normal text-lg ${
                                 userClaps > 0 ? 'text-rhodonite-500' : 'text-neutral-500'
                             }`}
                         >
-                            <FontAwesomeIcon icon={userClaps > 0 ? faHeartFilled : faHeartBordered} className='mr-2' />
+                            <FontAwesomeIcon
+                                icon={userClaps > 0 ? faHeartFilled : faHeartBordered}
+                                className='mr-2 group-active:scale-150 transition-transform ease-out'
+                            />
                             {totalClaps}
                         </span>
                         <span className='max-w-fit font-loopless font-normal text-lg text-neutral-500'>
@@ -30,13 +31,17 @@ function LikeSection({ className, articleId }) {
                     </span>
                     <span className='grow flex flex-wrap gap-4 justify-end items-center'>
                         <span className='font-loopless text-neutral-500'>
-                            <div className='text-right text-sm '>
+                            <div className='text-right text-sm'>
                                 {userClaps > 0 ? 'ขอบคุณค่ะ! 🙏' : 'ชอบบทความนี้ไหม?'}
                             </div>
                             <div className='text-xs'>
                                 {userClaps > 0 ? '' : 'อย่าลืมกดไลก์เป็นกำลังใจให้ผู้เขียนด้วยนะ'}
                             </div>
                         </span>
+                        <Thanks
+                            className='absolute opacity-0 fill-amethyst-300 -top-4 xs:-top-12 -right-6 w-16 
+                            group-active:opacity-100 group-active:-translate-y-3 transition-all ease-out duration-300'
+                        />
                         <Button
                             bordered
                             icon={{
